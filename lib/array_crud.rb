9c5c32b87db201d09b7406a2c17e882f2f14fa14
John Rogala <johnrossrogala@gmail.com>
2019-11-09 01:21:28 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,56 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
+  arrays = [4, 5, 6, 7]
   
 end
 
 def add_element_to_end_of_array(array, element)
+ number_poop = [1, 2, 50, 80, 70]
+ number_poop.push("arrays!")
+ 
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  smack = ["smooch", "toot", "foot", "boot"]
+  smack.unshift("wow")
 end
 
-def remove_element_from_end_of_array(array)
+def remove_element_from_end_of_array(array) 
+  doot = ["ten", "third", "arrays!"] 
+  second = doot.pop
+ 
   
 end
 
 def remove_element_from_start_of_array(array)
+  loot = ["wow", 6, 7, 8, 9] 
+  loot.shift 
   
 end
 
-def retrieve_element_from_index(array, index_number)
+def retrieve_element_from_index(array, index_number) 
+  truth = ["a", "am", "c", "d", "e"]
+  truth[1]
   
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(array) 
+  smooth = ["wow", 2, 3, 4, 5]
+  smooth[0]
 end
 
-def retrieve_last_element_from_array(array)
+def retrieve_last_element_from_array(array) 
+  night = [1, 2, 3, "arrays!"] 
+  night[3]
   
 end
 
 def update_element_from_index(array, index_number, element)
+  final = [1, 2, 3, 4]
+  final[0] = "totally"
 
 end
